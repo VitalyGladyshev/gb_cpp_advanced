4eb76bb2a7c397961b80e98dc05b760a284eb1c2
Vitaly Gladyshev <viv232@yandex.ru>
2022-04-26 21:58:19 +0300

    ДЗ 1 В процессе

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -5,8 +5,85 @@
 //
 
 #include <iostream>
+#include <tuple>
+#include <utility>
 
 using namespace std;
+
+//Структура Сотрудник
+struct Person
+{
+private:
+    string _lastName;       //фамилия
+    string _firstName;      //имя
+    string _patronymic;     //отчество
+public:
+    //Конструкторы
+    Person() : _lastName(""), _firstName(""), _patronymic("") {}
+    Person(string lastName, string firstName, string patronymic = "") :
+        _lastName(std::move(lastName)), _firstName(std::move(firstName)),
+        _patronymic(std::move(patronymic)) {}
+
+    //Прегружаем оператор <
+    friend bool operator< (const Person& person_1, const Person& person_2)
+    {
+        return tie(person_1._lastName, person_1._firstName, person_1._patronymic) <
+               tie(person_2._lastName, person_2._firstName, person_2._patronymic);
+    }
+    //Прегружаем оператор ==
+    friend bool operator== (const Person& person_1, const Person& person_2)
+    {
+        return tie(person_1._lastName, person_1._firstName, person_1._patronymic) ==
+                tie(person_2._lastName, person_2._firstName, person_2._patronymic);
+    }
+    //Прегружаем оператор <<
+    friend ostream& operator<< (ostream &out, const Person& person)
+    {
+        out << "\tСотрудник: " << person._lastName << " " << person._firstName << " " <<
+            person._patronymic << endl;
+        return out;
+    }
+};
+
+//Структура Номер телефона
+struct PhoneNumber
+{
+private:
+    int16_t _countryCode;       //код страны
+    int16_t _cityCode;          //код города
+    string _phoneNumber;        //номер телефона
+    int16_t _extensionNumber;   //добавочный номер
+public:
+    //Конструкторы
+    PhoneNumber() : _countryCode(0), _cityCode(0), _phoneNumber(""), _extensionNumber(0) {}
+    PhoneNumber(int16_t countryCode, int16_t cityCode, string phNumber,
+                int16_t extensionNumber) : _countryCode(countryCode), _cityCode(cityCode),
+                _phoneNumber(std::move(phNumber)), _extensionNumber(extensionNumber) {}
+
+    //Прегружаем оператор <
+    friend bool operator< (const PhoneNumber& phoneNumber_1, const PhoneNumber& phoneNumber_2)
+    {
+        return tie(phoneNumber_1._countryCode, phoneNumber_1._cityCode,
+                   phoneNumber_1._phoneNumber, phoneNumber_1._extensionNumber) <
+                tie(phoneNumber_2._countryCode, phoneNumber_2._cityCode,
+                    phoneNumber_2._phoneNumber, phoneNumber_2._extensionNumber);
+    }
+    //Прегружаем оператор ==
+    friend bool operator== (const PhoneNumber& phoneNumber_1, const PhoneNumber& phoneNumber_2)
+    {
+        return tie(phoneNumber_1._countryCode, phoneNumber_1._cityCode,
+                   phoneNumber_1._phoneNumber, phoneNumber_1._extensionNumber) ==
+               tie(phoneNumber_2._countryCode, phoneNumber_2._cityCode,
+                   phoneNumber_2._phoneNumber, phoneNumber_2._extensionNumber);
+    }
+    //Прегружаем оператор <<
+    friend ostream& operator<< (ostream &out, const PhoneNumber& phoneNumber)
+    {
+        out << "\tНомер телефона: " << phoneNumber._countryCode << " " << phoneNumber._cityCode
+            << " " << phoneNumber._phoneNumber << " " << phoneNumber._extensionNumber << endl;
+        return out;
+    }
+};
 
 int main()
 {
