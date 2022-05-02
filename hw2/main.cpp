715662a9a53f846960f9240a3e802edf7e5706f8
Vitaly Gladyshev <viv232@yandex.ru>
2022-05-02 19:04:41 +0300

    ДЗ 2 Начал

---
+++ hw2/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Гладышев ВВ on 02.05.2022.
+// C++: сложные моменты
+// ДЗ к уроку 2 Модель памяти и время выполнения
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+// Задание 1
+    cout << "Задание 1" << endl;
+
+// Задание 2
+    cout << "Задание 2" << endl;
+
+// Задание 3
+    cout << "Задание 3" << endl;
+
+
+}
