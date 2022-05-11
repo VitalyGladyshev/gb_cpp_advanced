32fcf50880d88ce83d889318e01b3847615af6bf
Vitaly Gladyshev <viv232@yandex.ru>
2022-05-11 17:36:35 +0300

    ДЗ 3 Начал

---
+++ hw3/main.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Гладышев ВВ on 11.05.2022.
+// C++: сложные моменты
+// ДЗ к уроку 3 Последовательные контейнеры STL
+//
+
+#include <iostream>
+#include <vector>
+#include "..\\Timer.h"
+
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+// Задание 1
+    cout << "Задание 1" << endl;
+//    Timer timer_fl("\t\tЗагрузка файла");
+//    timer_fl.print();
+
+    cout << endl;
+
+// Задание 2
+    cout << "Задание 2" << endl;
+
+    cout << endl;
+
+// Задание 3
+    cout << "Задание 3" << endl;
+
+}
