5803666571a7b7345129d4389f416c97e3353e70
Vitaly Gladyshev <viv232@yandex.ru>
2022-05-13 16:16:17 +0300

    ДЗ 4 начал

---
+++ hw4/main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Гладышев ВВ on 13.05.2022.
+// C++: сложные моменты
+// ДЗ к уроку 4 Алгоритмы STL
+//
+
+#include <iostream>
+#include "..\\Timer.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+// Задание 1
+    cout << "Задание 1" << endl;
+
+    Timer timer_t1("\tЗадание 1");
+
+    timer_t1.print();
+    cout << endl;
+
+// Задание 2
+    cout << "Задание 2" << endl;
+
+}
