42f1dc3b0444c6626809c19b2d339f5cb94a13ed
Vitaly Gladyshev <viv232@yandex.ru>
2022-05-22 16:07:50 +0300

    ДЗ 5 начал

---
+++ hw5/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Гладышев ВВ on 22.05.2022.
+// C++: сложные моменты
+// ДЗ к уроку 5 Алгоритмы STL
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+// Задание 1
+    cout << "Задание 1" << endl;
+
+    cout << endl;
+
+// Задание 2
+    cout << "Задание 2" << endl;
+
+    cout << endl;
+}
