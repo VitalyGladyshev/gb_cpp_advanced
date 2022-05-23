bbe0cbb722df7594b89cad9d0918426070d4e2b5
Vitaly Gladyshev <viv232@yandex.ru>
2022-05-24 01:03:02 +0300

    ДЗ 6 начал

---
+++ hw6/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Гладышев ВВ on 24.05.2022.
+// C++: сложные моменты
+// ДЗ к уроку 6 Параллельное программирование. Многопоточность
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+// Задание 1
+    cout << "Задание " << endl;
+
+    cout << endl;
+
+// Задание 2
+    cout << "Задание 2" << endl;
+
+    cout << endl;
+
+// Задание 3
+    cout << "Задание 3" << endl;
+
+
+}
