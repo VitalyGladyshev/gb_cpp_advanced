82bfefbf2f2a624b03b671282d176bef28e59c57
Vitaly Gladyshev <viv232@yandex.ru>
2022-05-29 19:09:24 +0300

    ДЗ 6 задания 1, 2

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -7,15 +7,16 @@
 
 #include <iostream>
 #include <sstream>
-#include<thread>
-#include<mutex>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <cmath>
+#include <condition_variable>
 #include "Timer.h"
 
 using namespace std;
 
 //Класс pcout
-
-
 class pcout: public ostringstream
 {
 public:
@@ -75,6 +76,51 @@ void func4()
     consol_color::SetColor(15, 0);
 }
 
+mutex mut;
+size_t curN;
+condition_variable data_cond;
+
+int GetPrimeNumber(int count)
+{
+    if (count > 0)
+    {
+        if (count > 1)
+        {
+            vector<int> primeNumbers {2};
+            for (int i = 3; primeNumbers.size() < count; i+=2)
+            {
+                bool prime = true;
+                int limit = ceil(i/2);
+                for (const auto pn : primeNumbers)
+                {
+                    if (!(i % pn))
+                    {
+                        prime = false;
+                        break;
+                    }
+                    if (pn > limit)
+                        break;
+                }
+                if (prime)
+                {
+                    primeNumbers.push_back(i);
+                    curN = primeNumbers.size();
+                    if(!(curN % 10))
+                    {
+                        lock_guard<mutex> lk(mut);
+                        data_cond.notify_one();
+                    }
+                }
+            }
+            return primeNumbers.back();
+        }
+        else
+            return 2;
+    }
+    else
+        return -1;
+}
+
 int main() {
     setlocale(LC_ALL, "Russian");
 // Задание 1
@@ -93,6 +139,30 @@ int main() {
 // Задание 2
     cout << "Задание 2" << endl;
 
+    Timer primeNumberTimer("\tПростое число N=100'000");
+    cout << "\tЧисло: " << GetPrimeNumber(100'000) << endl;
+    primeNumberTimer.print();
+
+    curN = 0;
+    size_t curNL = curN;
+    Timer primeNumberTimer2("\tПростое число N=100'000 в потоке");
+    thread prThr(GetPrimeNumber, 100'000);
+
+    while(true)
+    {
+        unique_lock<mutex> lk(mut);
+        data_cond.wait(lk, [&curNL] {return curNL != curN; });
+        curNL = curN;
+//        pcout{} << "\tНомер: " << curNL << endl;
+        cout << "\tНомер: " << curNL << "\r"; //endl;
+        cout.flush();
+        lk.unlock();
+        if (curNL == 100'000)
+            break;
+    }
+    cout << endl;
+    prThr.join();
+    primeNumberTimer2.print();
     cout << endl;
 
 // Задание 3
