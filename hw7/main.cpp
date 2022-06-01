ed4577c1ca8a858609fdb45860fb2982b18b0456
Vitaly Gladyshev <viv232@yandex.ru>
2022-06-01 18:10:07 +0300

    ДЗ 7 задание 1

---
+++ hw7/main.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Гладышев ВВ on 01.06.2022.
+// C++: сложные моменты
+// ДЗ к уроку 7. Пространство имен и бинарная серилизация
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "zlib.h"
+//#include "car.pb.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+// Задание 1
+//    cout << "Задание 1 (ставим и проверяем работоспособность protobuf)" << endl;
+//    cout << "\tCompiled with zlib " << ZLIB_VERSION << " using zlib " << zlib_version << endl;
+
+//    Car car;
+//    car.set_brand("Toyota");
+//    car.set_model("Camry");
+//    car.set_year(2011);
+//    cout << car.brand() << " " << car.model() << " " << car.year() << endl;
+//    ofstream out("/Users/semyon/Desktop/protobuf_lesson/car.bin", ios_base::binary);
+//    car.SerializeToOstream(&out);
+
+    cout << endl;
+
+// Задание 2
+
+    cout << endl;
+    return 0;
+}
